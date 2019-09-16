bfd263293a72b8df000163ddc2c7aae1c6fb93d6
钱苓苓 <qianlingling@haier.com>
2019-09-16 09:49:20 +0800

    Merge branch 'master' of https://github.com/lingerss/MachineLearning
    # Conflicts:
    #	DataModel/KNN_date.py

--- DataModel/DecisionTree/ID3.py
+++ DataModel/DecisionTree/ID3.py
@@ -1,6 +1,8 @@
 from math import log
 import operator
-import matplotlib.pyplot as plt
+from DataModel.DecisionTree import tree_plotter
+
+
 def create_data_set():
     """
     创建样本数据
@@ -17,7 +19,6 @@ def calc_shannon_ent(data_set):
     :return:
     """
     num = len(data_set)  # n rows
-    print('第一个:',num)
     # 为所有的分类类目创建字典
     label_counts = {}
     for feat_vec in data_set:
@@ -40,6 +41,7 @@ def split_data_set(data_set, axis, value):
     :param value: 分类值
     :return:
     """
+    print('axis:',axis,'value:',value)
     ret_data_set = []
     for feat_vec in data_set:
         if feat_vec[axis] == value:
@@ -58,9 +60,12 @@ def choose_best_feature_to_split(data_set):
     base_entropy = calc_shannon_ent(data_set) # 经验熵H(D)
     best_info_gain = 0
     best_feature_idx = -1
+    print('这是对特征值的检验：',range(num_feature))
     for feature_idx in range(num_feature):
         feature_val_list = [number[feature_idx] for number in data_set]  # 得到某个特征下所有值（某列）
+        print('获取某个特征下面的所有值：',feature_val_list)
         unique_feature_val_list = set(feature_val_list)  # 获取无重复的属性特征值
+        print("获取无重复的属性特征值：",unique_feature_val_list)
         new_entropy = 0
         for feature_val in unique_feature_val_list:
             sub_data_set = split_data_set(data_set, feature_idx, feature_val)
@@ -72,7 +77,74 @@ def choose_best_feature_to_split(data_set):
             best_info_gain = info_gain
             best_feature_idx = feature_idx
     return best_feature_idx
-
+def majority_cnt(class_list):
+    """
+    统计每个类别出现的次数，并按大到小排序，返回出现次数最大的类别标签
+    :param class_list: 类数组
+    :return:
+    """
+    class_count = {}
+    for vote in class_list:
+        if vote not in class_count.keys():
+            class_count[vote] = 0
+        class_count[vote] += 1
+    sorted_class_count = sorted(class_count.items(), key=operator.itemgetter(1), reversed=True)
+    print(sorted_class_count[0][0])
+    return sorted_class_count[0][0]
+def create_tree(data_set, labels):
+    """
+    构建决策树
+    :param data_set: 数据集合，如： [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
+    :param labels: 标签数组，如：['no surfacing', 'flippers']
+    :return:
+    """
+    class_list = [sample[-1] for sample in data_set] # ['yes', 'yes', 'no', 'no', 'no']
+    # 类别相同，停止划分
+    if class_list.count(class_list[-1]) == len(class_list):
+        return class_list[-1]
+    # 长度为1，返回出现次数最多的类别
+    if len(class_list[0]) == 1:
+        return majority_cnt((class_list))
+    # 按照信息增益最高选取分类特征属性
+    best_feature_idx = choose_best_feature_to_split(data_set)  # 返回分类的特征的数组索引
+    best_feat_label = labels[best_feature_idx]  # 该特征的label
+    my_tree = {best_feat_label: {}}  # 构建树的字典
+    del (labels[best_feature_idx])  # 从labels的list中删除该label，相当于待划分的子标签集
+    feature_values = [example[best_feature_idx] for example in data_set]
+    unique_feature_values = set(feature_values)
+    for feature_value in unique_feature_values:
+        sub_labels = labels[:]  # 子集合
+        # 构建数据的子集合，并进行递归
+        sub_data_set = split_data_set(data_set, best_feature_idx, feature_value) # 待划分的子数据集
+        my_tree[best_feat_label][feature_value] = create_tree(sub_data_set, sub_labels)
+    return my_tree
+def classify(input_tree, feat_labels, test_vec):
+    """
+    决策树分类
+    :param input_tree: 决策树
+    :param feat_labels: 特征标签
+    :param test_vec: 测试的数据
+    :return:
+    """
+    first_str = list(input_tree.keys())[0]  # 获取树的第一特征属性
+    second_dict = input_tree[first_str]  # 树的分子，子集合Dict
+    feat_index = feat_labels.index(first_str)  # 获取决策树第一层在feat_labels中的位置
+    for key in second_dict.keys():
+        if test_vec[feat_index] == key:
+            if type(second_dict[key]).__name__ == 'dict':
+                class_label = classify(second_dict[key], feat_labels, test_vec)
+            else:
+                class_label = second_dict[key]
+            return class_label
+    data_set, labels = create_data_set()
+    decision_tree = create_tree(data_set, labels)
+    print ("决策树：", decision_tree)
+    data_set, labels = create_data_set()
+    print ("（1）不浮出水面可以生存，无脚蹼：", classify(decision_tree, labels, [1, 0]))
+    print( "（2）不浮出水面可以生存，有脚蹼：", classify(decision_tree, labels, [1, 1]))
+    print ("（3）不浮出水面可以不能生存，无脚蹼：", classify(decision_tree, labels, [0, 0]))
+    tree_plotter.create_plot(decision_tree)
 if __name__=='__main__':
     data_set,labels=create_data_set()
     print(calc_shannon_ent(data_set))
+    print('最好的特征索引：',choose_best_feature_to_split(data_set))
